96f19e32266de0d65a5a197e257c398988c6eeea
illan-qualica <illan@qualica.com>
2012-06-07 23:49:55 +0200

    And now module2's turn

--- aggregator/module2/src/main/java/com/qualica/module2/Module2Class.java
+++ aggregator/module2/src/main/java/com/qualica/module2/Module2Class.java
@@ -6,7 +6,7 @@ import org.apache.log4j.Logger;
 public class Module2Class {
 	static Logger log = Logger.getLogger("Maven Parent");
 	public boolean doSomething() {
-		log.debug("I AM MODULE 2 - TWO - yes TWO  2 (2)");
+		log.debug("I AM MODULE 2 - TWO - yes TWO  2 (2) and also in GIT");
 		return true;
 	}
 	
